89d35cef6fea352aa15d1749407693ee122404f9
Jon Knopp <owner@jonknopp.com>
2020-03-03 17:58:30 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Gryfen
 Testing things...
+
+
+### Setup Azure Environment using Azure CLI & Terraform
+Launch the container which contains the Azure CLI and Terraform, passing it our initialization scirpt:
+
+```bash
+docker container run -it --rm --mount type=bind,source=$PWD,target=/workspace zenika/terraform-azure-cli:latest ./init_azure.sh
+```
--- init_azure.sh
+++ init_azure.sh
@@ -3,6 +3,7 @@
 #https://github.com/Zenika/terraform-azure-cli
 #docker container run -it --rm --mount type=bind,source="$PWD",target=/workspace zenika/terraform-azure-cli:latest ./init_azure.sh
 
+# Check if jq is installed, if not install it for parsing JSON
 echo "Installing dependencies ......"
 dpkg -l | grep -qw jq || apt-get update > /dev/null && apt-get install -y jq > /dev/null
 az login
